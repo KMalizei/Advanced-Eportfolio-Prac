3af755a66ac89217056218fa5a4a09708ecad307
KMalizei <107441437+KMalizei@users.noreply.github.com>
2022-07-13 17:48:39 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+let contrastToggle = false
+function toggleContrast(){
+    contrastToggle = !contrastToggle
+    if (contrastToggle){
+        document.body.classList += " dark-theme"
+    }
+    else{
+        document.body.classList.remove("dark-theme")
+    }
+   
+}
+
+function contact(event) {
+    event.preventDefault()
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible"
+        emailjs
+         .sendForm(
+            'service_7l3zev3',
+            'template_h9zs2gf',
+            event.target,
+            'Byc07zksSro7TEORC'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible")
+            success.classList += " modal__overlay--visible"
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible")
+            alert(
+                'Email Service is Temporarly unnavailable. Please Contact me directly on kmmalizei@gmail.com'
+            )
+        })      
+}
+
+let isModalOpen = false;
+function toggleModal(){
+    if (isModalOpen) {
+        isModalOpen = false
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true
+    document.body.classList += " modal--open"
+}
+
+const scaleFactor = 1/20
+
+function moveBackground(event){
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor
+    const y = event.clientY * scaleFactor
+
+    for (let i = 0; i < shapes.length; ++i){
+        const isOdd = i % 2 !== 0
+        const oddInteger = isOdd ? -1 : 1
+        shapes[i].style.transform = `translate(${x * oddInteger}px, ${y * oddInteger}px)`
+    }
+}
+
